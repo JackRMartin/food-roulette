c65fb78b02e1923b7e2052381b663176ecf68ad1
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-10-23 18:19:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ import tkinter as tk
 
 
 def random_rest():
-    choice_label.config(text = "You chose a random restaurant")
+    #choice_label.config(text = "You chose a random restaurant")
     random_button.config(state = "disabled")
     reset_button.config(state = "active")
+    for i in range(25):
+        results.insert(tk.END, keyword_entry.get())
 
 def reset():
-    choice_label.config(text = "")
+    #choice_label.config(text = "")
     random_button.config(state = "active")
     reset_button.config(state = "disabled")
-
-root = tk.Tk()
-root.title("Food-roulette")
-root.geometry("700x700+600+100")
-root.resizable(False, False)
+    results.delete(0, tk.END);
 
 
 ####################
 #                  #
+#   Create root    #
+#                  #
+####################
+
+root = tk.Tk()
+root.title("Food-roulette")
+root.geometry("700x475+600+100")
+root.resizable(False, False)
+
+
+####################
 #                  #
 #     Buttons      #
 #                  #
@@ -27,17 +36,17 @@ root.resizable(False, False)
 random_button = tk.Button(root, text = "Choose a random restaurant", command = random_rest)
 #random_button.grid(row=0, column=0)
 #random_button.pack(padx = 5, pady = 10, side = "bottom")
-random_button.place(x = 450, y = 650)
+random_button.place(x = 300, y = 350)
 
 reset_button = tk.Button(root, text = "reset", command = reset, state = "disabled")
 #reset_button.grid(row=0, column=1)
 #reset_button.pack(padx = 5, pady = 10, side = "bottom")
-reset_button.place(x = 600, y = 610)
+reset_button.place(x = 300, y = 380)
 
-choice_label = tk.Label(root, text="")
+#choice_label = tk.Label(root, text="")
 #choice_label.grid(row=0,column=2)
 #choice_label.pack(padx = 5, pady = 10, side = "bottom")
-choice_label.place(x = 10, y =10)
+#choice_label.place(x = 10, y =10)
 
 
 ####################
@@ -47,7 +56,7 @@ choice_label.place(x = 10, y =10)
 ####################
 
 filter_label = tk.Label(root, text = "Filter by category", relief = "ridge")
-filter_label.place(x = 525, y = 100)
+filter_label.place(x = 535, y = 100)
 
 delivery_btn = tk.Checkbutton(root, text = "Delivery Available")
 delivery_btn.place(x = 525, y = 130)
@@ -61,8 +70,41 @@ take_out_btn.place(x = 525, y = 190)
 alcohol_btn = tk.Checkbutton(root, text = "Serves Alcohol")
 alcohol_btn.place(x = 525, y = 220)
 
+''''
+price_label = tk.Label(root, text = "Price Range", relief = "ridge")
+price_label.place(x = 525, y = 275)
+
+price_scale = tk.Scale(root, from_=0, to_=100, orient = "horizontal")
+price_scale.place(x = 525, y = 300)
+'''
+
+####################
+#                  #
+# ListBox-results  #
+#                  #
+####################
+
+results_label = tk.Label(root, text = "Restaurant Results", relief = "ridge")
+results_label.place(x = 105, y = 2)
+
+results = tk.Listbox(root, height = 20, width = 30);
+results.place(x = 50, y = 50)
+
+results_scrollbar = tk.Scrollbar(results)
 
 
+
+####################
+#                  #
+#  Keyword entry   #
+#                  #
+####################
+
+keyword_label = tk.Label(root, text = "Keyword Search", relief = "ridge")
+keyword_label.place(x = 350, y = 25)
+
+keyword_entry = tk.Entry(root)
+keyword_entry.place(x = 325, y = 50)
 
 
 
