9cfa7744ae3b58667b6422096777db517796478c
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-11-01 17:53:06 -0700

    test.py, parsing JSON for location
    test.py, parsing JSON for location

---
+++ test.py
@@ -0,0 +1,26 @@
+'''
+import requests
+import socket
+
+hostname = socket.gethostname();
+
+
+r = requests.get("http://api.ipstack.com/137.151.174.24?access_key=98a13c3077a3992fd956e2a6f1b8dd43");
+
+print (r.content);
+'''
+from urllib.request import urlopen
+import json
+
+# Get the dataset
+base_url = 'http://api.ipstack.com/'
+ip = 1
+key = '?access_key=98a13c3077a3992fd956e2a6f1b8dd43'
+url = 'http://api.ipstack.com/137.151.174.24?access_key=98a13c3077a3992fd956e2a6f1b8dd43'
+response = urlopen(url)
+
+# Convert bytes to string type and string type to dict
+string = response.read().decode('utf-8')
+json_obj = json.loads(string)
+
+print(json_obj['city']) # prints the string with 'source_name' key
